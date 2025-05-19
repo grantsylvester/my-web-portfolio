cba477fc27d4e38a4720e2d5efe62b1237b042fb
grantsylvester <sylvestg@purdue.edu>
2025-05-18 21:07:39 -0400

    add project description dropdowns

--- scripts.js
+++ scripts.js
@@ -1,5 +1,27 @@
 let scrollButton;
 
+const hiddenProjects = [];
+
+function descDropdown(id) {
+    const itemsToToggle = document.getElementById(id).getElementsByTagName("p");
+    if(hiddenProjects.includes(id)) {
+        for(let i = 0; i < itemsToToggle.length; i++) {
+            itemsToToggle[i].style.display = "block";
+        }
+        removeHiddenProj(id);
+    } else {
+        for(let i = 0; i < itemsToToggle.length; i++) {
+            itemsToToggle[i].style.display = "none";
+        }
+        hiddenProjects.push(id);
+    }
+}
+
+function removeHiddenProj(id) {
+    let ind = hiddenProjects.indexOf(id);
+    hiddenProjects.splice(ind, 1);
+}
+
 function topFunction() {
     document.documentElement.scrollTop = 0;
     document.body.scrollTop = 0;
