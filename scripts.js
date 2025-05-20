8c5b14800a04e2816248da10f1f80f29dda53961
grantsylvester <sylvestg@purdue.edu>
2025-05-20 11:18:12 -0400

    Beautify dropdowns and project headers

--- scripts.js
+++ scripts.js
@@ -4,14 +4,17 @@ const hiddenProjects = [];
 
 function descDropdown(id) {
     const itemsToToggle = document.getElementById(id).getElementsByTagName("p");
+    const button = document.getElementById(id).getElementsByTagName("button")[0];
     if(hiddenProjects.includes(id)) {
         for(let i = 0; i < itemsToToggle.length; i++) {
             itemsToToggle[i].style.display = "block";
+            button.innerHTML = "Hide";
         }
         removeHiddenProj(id);
     } else {
         for(let i = 0; i < itemsToToggle.length; i++) {
             itemsToToggle[i].style.display = "none";
+            button.innerHTML = "Show";
         }
         hiddenProjects.push(id);
     }
